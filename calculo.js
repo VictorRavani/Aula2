1f19b0be28289efc6d0055334207c365d7c05cc2
VictorRavani <victorravani98@gmail.com>
2022-03-21 21:42:10 -0300

    Aula2 primeiro commit

---
+++ calculo.js
@@ -0,0 +1,33 @@
+ var n1 = 5;
+ var n2 = 5;
+ var n3 = 5;
+ var n4 = 8;
+ var total;
+ var media = 4;
+ var numero = 1;
+
+//total = ((n1+n2+n3+n4)/media);
+
+// if (total >= 7){
+ //    console.log("Aprovado " + total);
+
+// } else if (total >= 5 && total <= 7){
+ //   console.log("Recuperação " + total);
+// }
+
+// else {
+ //   console.log("Reprovado " + total);
+ //}
+
+ for(let numero = 1; numero <=10; numero ++){
+    console.log(numero);}
+ //total = n1 + n2;
+ //total = n1 - n2;
+ //total = n1 * n2;
+ //total = n1 / n2;
+ // total = n1 + n2 / n1;
+ //total = n1 % n2;
+ //total = n1 ** n2;
+ //var soma = n1 + n2 + n3 + n4 ;
+ //total = soma / 4;
+ //console.log(total)
---
+++ par.js
@@ -0,0 +1,17 @@
+const numero = 14;
+var x = 0;
+
+for(x = 0; x <= numero; x++)
+
+    if (x == 0){
+        console.log("número Zero" + x)
+    }
+    else if (x%2 == 0){
+        console.log("número é par" + x)
+    }
+    else {
+        console.log("número é impar" + x)
+    }
+
+
+
